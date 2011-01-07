ef7c7d5e52592934e59a133d3353837b8425178d
tordek.coast@gmail.com <Tordek@.(none)>
2011-01-07 21:56:42 -0200

    commit inicial

---
+++ Folha_de_Ponto.Dominio/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Folha_de_Ponto.Dominio
+{
+    public class Cliente
+    {
+        public String Nome;
+
+        public Cliente(String nome_do_cliente)
+        {
+            Nome = nome_do_cliente;
+        }
+    }
+}
---
+++ Folha_de_Ponto.Teste/Dado_Um_Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Folha_de_Ponto.Dominio;
+
+namespace Folha_de_Ponto.Teste
+{
+    [TestClass]
+    public class Dado_Um_Cliente
+    {
+        [TestMethod]
+        public void não_pode_criar_cliente_sem_nome()
+        {
+            String nome_do_cliente = "Grupo de Estudos";
+            Cliente cliente = new Cliente(nome_do_cliente);
+            Assert.AreEqual(nome_do_cliente, cliente.Nome);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void nome_do_cliente_não_pode_ser_vazio()
+        {
+            String nome_do_cliente = "";
+            Cliente cliente = new Cliente(nome_do_cliente);
+        }
+    }
+}
